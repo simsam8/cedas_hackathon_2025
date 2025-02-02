b1ed9a0304f7ccbe380bb906cb5e2898c47b720d
simsam8 <simvedaa@gmail.com>
2025-02-02 18:03:25 +0100

    update data prep functions

--- utils.py
+++ utils.py
@@ -4,19 +4,27 @@ import torch
 from torch.utils.data import TensorDataset
 
 
-def prep_time_series_data(df: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
+def prep_time_series_data(path, train_size=0.8):
     # TODO: figure out what to do with soc in prediction
     """
     train: soc, nominal_power, location_id, sub_id
     label: power
     """
+    df = pd.read_parquet(path)
+    df = df.drop("timestamp", axis=1)
+    # Interpolate soc
+    df = interpolate_soc(df)
+
+    df = df.dropna()
     df = df.groupby("id").agg(list)
     sessions = []
     labels = []
-    for i, row in df.drop("timestamp", axis=1).iterrows():
+    for i, row in df.iterrows():
         soc, power, nominal_power, location_id, sub_id = row
-        session = np.empty((40, 4))
-        label = np.empty((40))
+
+        # Pad to equal sequence length of 40
+        session = np.zeros((40, 4))
+        label = np.zeros((40))
         session_length = len(soc)
         for i in range(session_length):
             session[i] = np.array([soc[i], nominal_power[i], location_id[i], sub_id[i]])
@@ -25,11 +33,25 @@ def prep_time_series_data(df: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
         sessions.append(session)
         labels.append(label)
 
-    sessions = np.array(sessions)
-    labels = np.array(labels)
+    x = np.array(sessions)
+    y = np.array(labels)
+
+    split = int(len(x) * train_size)
+    x_train, y_train = x[:split], y[:split]
+    x_val, y_val = x[split:], y[split:]
+
+    train = TensorDataset(
+        torch.tensor(x_train, dtype=torch.float32),
+        torch.tensor(y_train, dtype=torch.float32),
+    )
+
+    val = TensorDataset(
+        torch.tensor(x_val, dtype=torch.float32),
+        torch.tensor(y_val, dtype=torch.float32),
+    )
 
     # np.save("train_data.npy", sessions)
-    return sessions, labels
+    return train, val
 
 
 def interpolate_soc(df):
@@ -44,28 +66,40 @@ def interpolate_soc(df):
     return df
 
 
-def prep_data(path, train_size=0.8):
+def prep_data(
+    path, do_split=False, train_size=0.8, as_torch_data=False, eval_set=False
+):
     df = pd.read_parquet(path)
     df = df.drop("timestamp", axis=1)
 
     # Interpolate soc
     df = interpolate_soc(df)
 
-    df = df.dropna()
+    if not eval_set:
+        df = df.dropna()
+    # df["power"] = df["power"].replace(np.nan, -1)
     x, y = df[["id", "soc", "nominal_power", "location_id", "sub_id"]], df[["power"]]
     split = int(len(x) * train_size)
 
     x, y = np.array(x), np.array(y)
 
+    if not do_split:
+        return x, y
+
     x_train, y_train = x[:split], y[:split]
     x_val, y_val = x[split:], y[split:]
-    train = TensorDataset(
-        torch.tensor(x_train, dtype=torch.float32),
-        torch.tensor(y_train, dtype=torch.float32),
-    )
-    val = TensorDataset(
-        torch.tensor(x_val, dtype=torch.float32),
-        torch.tensor(y_val, dtype=torch.float32),
-    )
+
+    if as_torch_data:
+        train = TensorDataset(
+            torch.tensor(x_train, dtype=torch.float32),
+            torch.tensor(y_train, dtype=torch.float32),
+        )
+        val = TensorDataset(
+            torch.tensor(x_val, dtype=torch.float32),
+            torch.tensor(y_val, dtype=torch.float32),
+        )
+    else:
+        train = (x_train, y_train)
+        val = (x_val, y_val)
 
     return train, val
